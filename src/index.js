6b3b620b4b545542e9938435d4ef437ea55cd7e0
Robert Burnham <burnhamrobertp@gmail.com>
2019-02-15 08:27:48 -0600

    Rename index.jsx back to index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import * as serviceWorker from './serviceWorker'
 import './index.scss'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+)
 
 ReactDOM.render(
   <Provider store={store}>
