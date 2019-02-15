012cd474c61d32b72f6b84a8644fa92cb6b45296
Robert Burnham <burnhamrobertp@gmail.com>
2019-02-14 22:46:33 -0600

    Add thunk, remove prettier for now

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './state/root-reducer'
@@ -8,13 +9,13 @@ import * as serviceWorker from './serviceWorker'
 import './index.scss'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <Pivocle />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
 
 // If you want your app to work offline and load faster, you can change
